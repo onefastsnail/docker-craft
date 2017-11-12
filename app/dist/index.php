f4330d7ffcf4c139f7f35d68aebf04e1d42df25a
Paul Stewart <onefastsnail@gmail.com>
2017-11-12 22:18:53 +0200

    Added Craft CMS, will change this through Composer later. Tweaked Craft config to use the env vars

--- app/dist/index.php
+++ app/dist/index.php
@@ -1,12 +1,19 @@
 <?php
-$servername = getenv('APP_DB_HOST');
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo '<h1 style="text-align:center;">Database connection successfull!</h1>';
-echo phpinfo();
+
+// Path to your craft/ folder
+$craftPath = '../craft';
+
+// Do not edit below this line
+$path = rtrim($craftPath, '/').'/app/index.php';
+
+if (!is_file($path))
+{
+	if (function_exists('http_response_code'))
+	{
+		http_response_code(503);
+	}
+
+	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+}
+
+require_once $path;
